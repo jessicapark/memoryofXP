05d4df3b7b8620128be6698b296b23a729e12ac6
jessicapark <sietsarr.cc@gmail.com>
2014-04-02 04:40:25 +0800

    作死不是两三天

---
+++ script/load.js
@@ -0,0 +1,33 @@
+﻿$(function(){
+	var beforeDate = new Date();
+	//页面加载
+	var $window = $(window),
+		$doc = $(document),
+		$body = $("body"),
+	    winWidth = $window.width(),
+		docWidth = $doc.width(),
+		docHeight = $doc.height(),
+		winHeight = $window.height(),	
+		speed = 250;
+	var afterDate = new Date(),
+	    pagePreLoad = (afterDate - beforeDate),
+	    time = 10/pagePreLoad,
+		preImgLen = 35/pagePreLoad,
+		n = 0,
+		m = 0,
+		play = setInterval(function(){
+			if(Number(n) >= 100 && Number(m) >= 350) { 
+				clearInterval(play); 
+				n = 100;
+				m = 350;			
+				//页面加载完毕后执行(主线)
+				setTimeout(function(){
+					$(this).remove();
+					window.scrollTo(0,17000);
+					document.getElementById('loading').style.display = 'none';
+				},2000);
+			};
+			n += time;
+			m += preImgLen;
+		},100);
+});
