adc531f2ca1639e46c7559769b0fe26f1ee5bfd6
jessicapark <sietsarr.cc@gmail.com>
2014-04-04 16:29:21 +0800

    最后一次切白！

---
+++ script/page-scroll.js
@@ -0,0 +1,19 @@
+/**
+ * @author Administrator
+ */
+$(document).ready(function() {
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length && target || $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                var targetOffset = target.offset().top;
+                ('html,body').animate({
+                    scrollTop: targetOffset
+                },
+               0.0005);
+                return false;
+            }
+        }
+    });
+});
